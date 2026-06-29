4465c04eb1540c37d775d35ca0ca8f11bad433c3
Noop04 <145244598+Noop04@users.noreply.github.com>
2026-06-29 13:08:45 -0500

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.cpp
@@ -0,0 +1,12 @@
+#include <string>
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int x = nums.size();
+        for (int i = 0; i < nums.size(); i++) {
+            x ^= i ^ nums[i];
+        }
+        return x;
+        
+    }
+};
